e7f0ebfde875dc4e4e2c112dee8e983b64bd6ad9
김도영 <kimdo489@nts-corp.com>
2019-12-24 17:56:48 +0900

    Add typescript React

--- frontend/src/js/main.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-// Render main app
-ReactDOM.render(<App/>, document.getElementById('app'));
