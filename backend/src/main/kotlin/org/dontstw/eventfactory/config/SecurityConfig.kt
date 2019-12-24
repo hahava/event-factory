21f24ecfb058dcd41261aeb931a30ce4e4992848
hahava <hahava5696@gmail.com>
2019-12-24 14:39:39 +0900

    Add security config
    - Allowing request with start "/api/*"

---
+++ backend/src/main/kotlin/org/dontstw/eventfactory/config/SecurityConfig.kt
@@ -0,0 +1,16 @@
+package org.dontstw.eventfactory.config
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        http!!.authorizeRequests()
+                .antMatchers("/api")
+                .permitAll()
+    }
+
+}
